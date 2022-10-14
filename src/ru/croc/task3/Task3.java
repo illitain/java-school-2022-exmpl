88f06023e8389b2eb0512dbbe26a149e404ff821
Den <denrus1995@gmail.com>
2022-10-14 14:54:18 +0300

    И ещё обновление имен классов

--- src/ru/croc/task3/Task3.java
+++ src/ru/croc/task3/Task3.java
@@ -2,7 +2,7 @@ package ru.croc.task3;
 
 import ru.croc.task3.some_package.SomeClass;
 
-public class Main {
+public class Task3 {
     public static void main(String[] args) {
         SomeClass.printSome();
     }
--- src/ru/croc/task4/Task4.java
+++ src/ru/croc/task4/Task4.java
@@ -2,7 +2,7 @@ package ru.croc.task4;
 
 import ru.croc.task4.some_package.SomeClass;
 
-public class Main {
+public class Task4 {
     public static void main(String[] args) {
         SomeClass.printSome();
     }
